e480a351d8434db54cdc4e434d1509247c99ec16
Divyam <divyamrajpandey220@gmail.com>
2025-09-25 04:18:15 +0530

    made correct indentation code.

--- main.cpp
+++ main.cpp
@@ -14,44 +14,36 @@ struct Node{
         this->text = text;
         this->children = c;
    }
-   int t = 0;
-
 
    void __tagAndText__(string tag , string text){
     cout << "<"<<tag<<">"<<text<<"<"<<tag<<">"<<endl;
    }
 
    
-   void __tree__(Node a){
-        queue<Node> q;
-        q.push(a);
-        t++;
-        while(!q.empty()){
-            auto x = q.front();
-            t++;
-            q.pop();
+   void __tree__(Node x,int depth){
+            for(int i = 0; i < depth * 2; i++)
+                cout << " ";
+                __tagAndText__(x.tag_name , x.text);    
             for(auto a : x.children){
-                for(int i=0;i<t*2;i++)cout<<" ";
-                __tagAndText__(a.tag_name , a.text);
-                q.push(a);
+                __tree__(a,depth+1);
             }
-        }
         return;
-   }
+    }
+   
 };
 
 int main(){
 
-    Node div("p","p inside div",{});
-    Node div4("div4","div4 inside div3",{div,div});
-    Node div5("div5","div5 inside div3",{div,div});
-    Node div6("div6","div6 inside div3",{div});
-    Node div3("div3","div3 inside div2",{div4,div,div,div,div,div5,div6});
-    Node div2("div2","div2 inside header",{div3,div});
-    Node div1("div1","div1 inside header",{div,div});
-    Node html("h1","header",{div1, div2,div});
+   
+    Node div4("div4","div4 inside div3",{});
+    Node div5("div5","div5 inside div3",{});
+    Node div6("div6","div6 inside div3",{});
+    Node div3("div3","div3 inside div2",{div4,div5,div6});
+    Node div2("div2","div2 inside header",{div3});
+    Node div1("div1","div1 inside header",{});
+    Node html("h1","header",{div1, div2});
     Node root("root","root",{html});
-    root.__tree__(root);
+    root.__tree__(root,0);
 
     return 0;
 }
